fab03b7cffadd9765abfc1ce45b9fc405a710f90
Ji Chang <mrchangji@outlook.com>
2021-03-23 17:07:56 +0800

    fix typo and add placeholder text for previewing

--- docs/standard/index.md
+++ docs/standard/index.md
@@ -1,2 +1,3 @@
 [English Document](./standard.en-US.md)
+
 [中文文档](./standard.zh-Hans.md)
--- src/FSharp.ScreenPlayer.Fable/src/App.fs
+++ src/FSharp.ScreenPlayer.Fable/src/App.fs
@@ -15,6 +15,34 @@ Fable.Core.JsInterop.importAll "./App.css"
 
 let draftStorageKey = "screenplayer.draft"
 
+let placeholder = """${Title}={PULP FICTION}
+${Authors}={Quentin Tarantino; Roger Avary}
+
+[INT. COFFEE SHOP – MORNING]
+
+A normal Denny's, Spires-like coffee shop in Los Angeles. It's about 9:00 in the morning. While the place isn't jammed, there's a healthy number of people drinking coffee, munching on bacon and eating eggs.
+
+Two of these people are a YOUNG MAN and a YOUNG WOMAN. The Young Man has a slight working-class English accent and, like his fellow countryman, smokes cigarettes like they're going out of style.
+
+It is impossible to tell where the Young Woman is from or how old she is; everything she does contradicts something she did. The boy and girl sit in a booth. Their dialogue is to be said in a rapid pace "HIS GIRL FRIDAY" fashion.
+
+@{YOUNG MAN} "No, forget it, it's too risky. I'm through doin' that shit."
+
+@{YOUNG WOMAN} "You always say that, the same thing every time: never again, I'm through, too dangerous."
+
+@{YOUNG MAN} "I know that's what I always say. I'm always right too, but –"
+
+@{YOUNG WOMAN} "– but you forget about it in a day or two -"
+
+@{YOUNG MAN} "– yeah, well, the days of me forgittin' are over, and the days of me rememberin' have just begun."
+
+@{YOUNG WOMAN} "When you go on like this, you know what you sound like?"
+
+@{YOUNG MAN} "I sound like a sensible fucking man, is what I sound like."
+
+@{YOUNG WOMAN} "You sound like a duck."(imitates a duck)"Quack, quack, quack, quack, quack, quack, quack..."
+"""
+
 type Mode =
     | Preview
     | Edit
@@ -36,10 +64,27 @@ let filterLinebreak (line: Line) =
     | _ -> true
 
 let init () =
-    let content = localStorage.getItem draftStorageKey
+    let cache = localStorage.getItem draftStorageKey
+    let content =
+        if isNull cache then
+            placeholder
+        else
+            cache
+
+    let lines =
+        let source =
+            { line = 0
+              offset = 0
+              chars = content }
+
+        match parse [||] source with
+        | Ok lines -> lines |> Seq.filter filterLinebreak
+        | Error error ->
+            printfn "%A" error
+            Seq.empty
 
     { editorModel = Editor.init "ScreenPlayer" content
-      playerModel = Player.init []
+      playerModel = Player.init lines
       mode = Edit },
     Cmd.none
 
@@ -59,7 +104,9 @@ let update (msg: Msg) (model: Model) =
 
                 match parse [||] source with
                 | Ok lines -> lines |> Seq.filter filterLinebreak
-                | _ -> model.playerModel.lines
+                | Error error ->
+                    printfn "%A" error
+                    model.playerModel.lines
 
             let playerModel =
                 { model.playerModel with
